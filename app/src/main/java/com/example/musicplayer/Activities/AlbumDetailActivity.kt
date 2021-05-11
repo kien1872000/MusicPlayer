bf91c84ced38886844d6e01572d6518e34091866
kien1872000 <kkien.18700@gmail.com>
2021-05-11 10:10:30 +0700

    update

---
+++ app/src/main/java/com/example/musicplayer/Activities/AlbumDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.musicplayer.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_detail)
+    }
+}
---
+++ app/src/main/java/com/example/musicplayer/Models/Album.kt
@@ -0,0 +1,4 @@
+package com.example.musicplayer.Models
+
+class Album {
+}
